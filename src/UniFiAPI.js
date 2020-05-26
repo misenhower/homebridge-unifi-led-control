e8bbeaabc1ba0f2b6fa9c1e9c1d3de620e91dd9b
Matt Isenhower <matt@isenhower.com>
2020-05-26 16:45:08 -0700

    Add UDM support

--- src/UniFiAPI.js
+++ src/UniFiAPI.js
@@ -31,12 +31,19 @@ module.exports = class UniFiAPI {
     }
   }
 
-  async _performRequest(method, url, data = null) {
-    const config = {
+  async _performRequest(method, url, data = null, config = null) {
+    let headers = {};
+    if (this.csrf) {
+      headers['x-csrf-token'] = this.csrf;
+    }
+
+    config = {
       method,
       url,
       data,
       baseURL: this.url,
+      headers,
+      ...config,
     };
 
     this.log.debug(`Performing request: ${method} ${url}`);
@@ -46,11 +53,42 @@ module.exports = class UniFiAPI {
 
     this.log.debug(`Response: ${JSON.stringify(result.data)}`);
 
-    return result.data;
+    let csrf = result.headers['x-csrf-token'];
+    if (csrf) {
+      this.csrf = csrf;
+    }
+
+    return result;
+  }
+
+  async checkMode() {
+    // Check whether we are connecting to a UDM or an older controller.
+    // The UDM responds with HTTP code 200 when you peform a GET request to /,
+    // while older controllers redirect you to /manage.
+    // This detection method was adapted from unifi-poller.
+
+    this.isNewMode = false;
+
+    try {
+      let response = await this._performRequest('GET', '/', null, { maxRedirects: 0, validateStatus: false });
+
+      if (response.status === 200) {
+        this.isNewMode = true;
+      }
+    } catch (e) {
+      //
+    }
   }
 
   async login() {
-    await this._performRequest('POST', 'api/login', {
+    await this.checkMode();
+
+    // The api/auth/login will return a 404 if we already have login cookies.
+    this.axios.cookieJar.removeAllCookiesSync();
+
+    let url = (this.isNewMode) ? 'api/auth/login' : 'api/login';
+
+    await this._performRequest('POST', url, {
       username: this.username,
       password: this.password,
     });
@@ -58,15 +96,22 @@ module.exports = class UniFiAPI {
     this.log.info('Successfully logged into UniFi controller');
   }
 
-  getSites() {
-    return this.request('GET', 'api/self/sites');
+  _prefixUrl(url) {
+    return (this.isNewMode) ? `proxy/network/${url}` : url;
   }
 
-  getDevices(site) {
-    return this.request('GET', `api/s/${site}/stat/device`);
+  async getSites() {
+    let response = await this.request('GET', this._prefixUrl('api/self/sites'));
+    return response.data;
   }
 
-  setDevice(site, deviceId, data) {
-    return this.request('PUT', `api/s/${site}/rest/device/${deviceId}`, data);
+  async getDevices(site) {
+    let response = await this.request('GET', this._prefixUrl(`api/s/${site}/stat/device`));
+    return response.data;
+  }
+
+  async setDevice(site, deviceId, data) {
+    let response = await this.request('PUT', this._prefixUrl(`api/s/${site}/rest/device/${deviceId}`), data);
+    return response.data;
   }
 };
